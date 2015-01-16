92c797d04a38ac3a04e8993f8995d33135f80ff4
cloudeng <cloudeng@cvcwsd01.dtcc.com>
2015-01-16 11:11:38 -0500

    update

--- app/models/host.rb
+++ app/models/host.rb
@@ -48,7 +48,62 @@ class Host < ActiveRecord::Base
 	instance_volumes = Instance.volumes(host.instance.instance_id)
 	instance_volumes.each do |vol|
 		 ec2.detach_volume(instance_id: host.instance.instance_id, volume_id: vol, force: true)
+	enddef self.replace_host(host_id)
+	host = Host.include_all.find(host_id)
+	instance = host.instance
+	region = host.instance.availability_zone.chop
+	aws_account = AwsAccount.find_by_account_number(host.environment.aws_account_id)
+	ec2 = setup_ec2(aws_account.id, region)
+	instance_status = ec2.describe_instance_status(instance_ids: [host.instance.instance_id]).instance_statuses.first.instance_state.name
+	eni = InstanceEniMapping.where(instance_id: host.instance.instance_id)
+	eni_attachment_id = InstanceEni.find_by_network_interface_id(eni.first.network_interface_id).attachtment_id
+	security_group = InstanceSecurityGroupMapping.where(instance_id: instance.instance_id).first.group_id
+
+	
+	# build mapping of drives 
+	###
+	instance_volumes = Hash.new
+	InstanceBlockDeviceMapping.where(instance_id: host.instance.instance_id).each do |map| 
+		instance_volumes["#{map.device_name}"] = map.volume_id
 	end
+	#
+	# Attempt Stop of instance if runnning
+	if instance_status == "running"
+		sleep 1 until ["stopped"].include?(ec2.stop_instances(instance_ids: [host.instance.instance_id]).stopping_instances.first.current_state.name)
+	end
+	
+	instance_volumes.each do |mount,vol|
+		 ec2.detach_volume(instance_id: host.instance.instance_id, volume_id: vol, force: true
+	end
+	sleep 1 untill ["terminated"].include?(ec2.terminate_instances(instance_ids: [host.instance.instance_id]).terminating_instances.first.current_state.name)
+	
+	resp = Instance.create_instance(host.aws_account.id, instance.availability_zone,instance.instance_type,instance.key_name,sg,instance.subnet_id,instance.private_ip_address)
+	host.instance_id = resp.instances.first.instance_id
+	host.save
+	
+	instance.instance_id = resp.instances.first.instance_id
+	instance.save
+	
+	sleep 1 until ["running"].include?(ec2.describe_instance_status(instance_ids: [host.instance.instance_id]).instance_statuses.first.instance_state.name)
+	sleep 1 until ["stopped"].include?(ec2.stop_instances(instance_ids: [host.instance.instance_id]).stopping_instances.first.current_state.name)
+	base_vol =  ec2.describe_instances(instance_ids: [host.instance.instance_id]).reservations.first.instances.first.block_device_mappings.first.ebs.volume_id
+	ec2.detach_volume(instance_id: host.instance.instance_id, volume_id: base_vol, force: true)
+	sleep 5 
+	ec2.delete_volume(volume_id: base_vol)
+	
+	instance_volumes.each do |mount,vol|
+		 ec2.attach_volume(instance_id: host.instance.instance_id, volume_id: vol, device: mount)
+	end
+	Instance.add_update_tag(host.instance_id,"Name",host.hostname)
+	Instance.add_update_tag(host.instance_id,"SYSID",host.sysid.name)
+	Instance.update_instances
+    Instance.update_volume_tags(host.instance_id,"Name",host.hostname)
+    Instance.update_volume_tags(host.instance_id,"SYSID",host.sysid.name)
+	Instance.update_volume_tags(host.instance_id,"host_id",host.id)
+	ec2.start_instances(instance_ids: [instance.instance_id])
+	 
+	 
+  end
 	
   end
   
